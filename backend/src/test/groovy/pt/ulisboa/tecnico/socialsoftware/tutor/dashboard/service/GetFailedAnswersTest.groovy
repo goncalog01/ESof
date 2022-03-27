1a8ab5adc315fa494080817c8f3f1250201ab4dd
ImGugz <gustavoaaguiar@tecnico.ulisboa.pt>
2022-03-27 21:37:52 +0100

    closes #50; added get service test base file with no tests

---
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/dashboard/service/GetFailedAnswersTest.groovy
@@ -0,0 +1,38 @@
+package pt.ulisboa.tecnico.socialsoftware.tutor.dashboard.service
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.context.TestConfiguration
+import pt.ulisboa.tecnico.socialsoftware.tutor.BeanConfiguration
+import pt.ulisboa.tecnico.socialsoftware.tutor.auth.domain.AuthUser
+import pt.ulisboa.tecnico.socialsoftware.tutor.dashboard.domain.Dashboard
+import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.ErrorMessage
+import pt.ulisboa.tecnico.socialsoftware.tutor.exceptions.TutorException
+import pt.ulisboa.tecnico.socialsoftware.tutor.user.domain.Student
+import pt.ulisboa.tecnico.socialsoftware.tutor.utils.DateHandler
+import spock.lang.Unroll
+
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+@DataJpaTest
+class GetFailedAnswersTest extends FailedAnswersSpockTest {
+    def quiz
+    def quizQuestion
+
+    def setup() {
+        createExternalCourseAndExecution()
+
+        student = new Student(USER_1_NAME, USER_1_USERNAME, USER_1_EMAIL, false, AuthUser.Type.TECNICO)
+        student.addCourse(externalCourseExecution)
+        userRepository.save(student)
+
+        dashboard = new Dashboard(externalCourseExecution, student)
+        dashboardRepository.save(dashboard)
+
+        quiz = createQuiz(1)
+        quizQuestion = createQuestion(1, quiz)
+    }
+
+    @TestConfiguration
+    static class LocalBeanConfiguration extends BeanConfiguration {}
+}
