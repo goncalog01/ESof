be313241b8fcc28d90bc0fceea09569a01493f0c
ImGugz <gustavoaaguiar@tecnico.ulisboa.pt>
2022-03-29 04:04:26 +0100

    #107; removed useless imports

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/dashboard/FailedAnswerService.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/dashboard/FailedAnswerService.java
@@ -1,8 +1,6 @@
 package pt.ulisboa.tecnico.socialsoftware.tutor.dashboard;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.retry.annotation.Backoff;
-import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
