f1aa6bd4540d425de9918d12363d68efc04ef787
ImGugz <gustavoaaguiar@tecnico.ulisboa.pt>
2022-03-29 20:54:49 +0100

    closes #124; added student gets failed answers test

---
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/dashboard/webservice/GetFailedAnswersWebServiceIT.groovy
@@ -0,0 +1,105 @@
+package pt.ulisboa.tecnico.socialsoftware.tutor.dashboard.webservice
+
+import groovyx.net.http.HttpResponseException
+import groovyx.net.http.RESTClient
+import org.apache.http.HttpStatus
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.web.server.LocalServerPort
+
+import pt.ulisboa.tecnico.socialsoftware.tutor.auth.domain.AuthUser
+import pt.ulisboa.tecnico.socialsoftware.tutor.dashboard.domain.Dashboard
+import pt.ulisboa.tecnico.socialsoftware.tutor.dashboard.service.FailedAnswersSpockTest
+import pt.ulisboa.tecnico.socialsoftware.tutor.user.domain.Student
+
+import java.time.LocalDateTime
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class GetFailedAnswersWebServiceIT extends FailedAnswersSpockTest {
+    @LocalServerPort
+    private int port
+
+    def response
+    def quiz
+    def quizQuestion
+
+    def setup() {
+        given:
+        restClient = new RESTClient("http://localhost:" + port)
+        and:
+        createExternalCourseAndExecution()
+        and:
+        student = new Student(USER_1_NAME, USER_1_USERNAME, USER_1_EMAIL, false, AuthUser.Type.EXTERNAL)
+        student.authUser.setPassword(passwordEncoder.encode(USER_1_PASSWORD))
+        student.addCourse(externalCourseExecution)
+        userRepository.save(student)
+        and:
+        dashboard = new Dashboard(externalCourseExecution, student)
+        dashboardRepository.save(dashboard)
+
+        and:
+        quiz = createQuiz(1)
+        quizQuestion = createQuestion(1, quiz)
+    }
+
+    def "student gets failed answers"() {
+        given: "login student create 3 failed answers"
+        createdUserLogin(USER_1_USERNAME, USER_1_PASSWORD)
+        def quiz2 = createQuiz(2)
+        def quiz3 = createQuiz(3)
+
+        def quizQuestion2 = createQuestion(2, quiz2)
+        def quizQuestion3 = createQuestion(3, quiz3)
+
+        def questionAnswer = answerQuiz(true, false, true, quizQuestion, quiz)
+        def questionAnswer2 = answerQuiz(true, false, true, quizQuestion2, quiz2)
+        def questionAnswer3 = answerQuiz(true, false, true, quizQuestion3, quiz3)
+
+        def failedAnswer1Dto = failedAnswerService.createFailedAnswer(dashboard.getId(), questionAnswer.getId())
+        def failedAnswer2Dto = failedAnswerService.createFailedAnswer(dashboard.getId(), questionAnswer2.getId())
+        def failedAnswer3Dto = failedAnswerService.createFailedAnswer(dashboard.getId(), questionAnswer3.getId())
+
+        when: "the web service is invoked"
+        response = restClient.get(
+                path: '/students/failedanswers/' + dashboard.getId(),
+                requestContentType: 'application/json'
+        )
+
+        then: "the request returns 200"
+        response != null
+        response.status == 200
+
+        and: "the repository contains both failed answers"
+        failedAnswerRepository.findAll().size() == 3
+
+        and: "dashboard contains same failed answers"
+        def info = response.data
+        info.get(0).questionAnswerDto.question.id == failedAnswer3Dto.getQuestionAnswerDto().getQuestion().getId()
+        info.get(1).questionAnswerDto.question.id == failedAnswer2Dto.getQuestionAnswerDto().getQuestion().getId()
+        info.get(2).questionAnswerDto.question.id == failedAnswer1Dto.getQuestionAnswerDto().getQuestion().getId()
+
+        info.get(0).questionAnswerDto.answerDetails.option.id == failedAnswer3Dto.getQuestionAnswerDto().getAnswerDetails().getOption().getId()
+        info.get(1).questionAnswerDto.answerDetails.option.id == failedAnswer2Dto.getQuestionAnswerDto().getAnswerDetails().getOption().getId()
+        info.get(2).questionAnswerDto.answerDetails.option.id == failedAnswer1Dto.getQuestionAnswerDto().getAnswerDetails().getOption().getId()
+
+        cleanup:
+        failedAnswerRepository.deleteAll()
+        questionAnswerRepository.deleteById(questionAnswer.getId())
+        questionAnswerRepository.deleteById(questionAnswer2.getId())
+        questionAnswerRepository.deleteById(questionAnswer3.getId())
+    }
+
+    def "teacher can't get student's failed answers"() {
+
+    }
+
+    def "student can't get another student's failed answers"() {
+
+    }
+
+    def cleanup() {
+        userRepository.deleteById(student.getId())
+        courseExecutionRepository.deleteById(externalCourseExecution.getId())
+        courseRepository.deleteById(externalCourseExecution.getCourse().getId())
+    }
+
+}
