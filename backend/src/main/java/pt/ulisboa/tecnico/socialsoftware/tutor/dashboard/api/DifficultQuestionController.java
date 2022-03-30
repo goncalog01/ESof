7da25b27fcb7498a441f8c5d32e9e513783989f4
Sebastiao-Assuncao <sebastiao.assuncao@gmail.com>
2022-03-30 16:00:16 +0100

    closes #134; Implemented new deleteDifficultQuestion REST web service

---
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/dashboard/api/DifficultQuestionController.java
@@ -0,0 +1,29 @@
+package pt.ulisboa.tecnico.socialsoftware.tutor.dashboard.api;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import pt.ulisboa.tecnico.socialsoftware.tutor.dashboard.DifficultQuestionService;
+
+@RestController
+public class DifficultQuestionController {
+    private static final Logger logger = LoggerFactory.getLogger(DashboardController.class);
+
+    @Autowired
+    private DifficultQuestionService difficultQuestionService;
+
+    DifficultQuestionController(DifficultQuestionService difficultQuestionService) {
+        this.difficultQuestionService = difficultQuestionService;
+    }
+
+    @PutMapping("/students/dashboards/difficultquestions/{difficultQuestionId}")
+    @PreAuthorize("hasRole('ROLE_STUDENT')")
+    public void deleteDifficultQuestion(@PathVariable int difficultQuestionId) {
+        this.difficultQuestionService.removeDifficultQuestion(difficultQuestionId);
+    }
+}
