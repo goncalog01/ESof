d627c5907c4595bbbaa4174c7e42a3580e9542ec
Tomás Tavares <tomastavares21122001@gmail.com>
2022-03-31 15:31:02 +0100

    closes #159; added remove failed anwers base test file

---
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/dashboard/webservice/RemoveFailedAnswersWebServiceIT.groovy
@@ -0,0 +1,58 @@
+package pt.ulisboa.tecnico.socialsoftware.tutor.dashboard.webservice
+
+import groovyx.net.http.HttpResponseException
+import groovyx.net.http.RESTClient
+import org.apache.http.HttpStatus
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.web.server.LocalServerPort
+import pt.ulisboa.tecnico.socialsoftware.tutor.auth.domain.AuthUser
+import pt.ulisboa.tecnico.socialsoftware.tutor.dashboard.domain.Dashboard
+import pt.ulisboa.tecnico.socialsoftware.tutor.dashboard.service.FailedAnswersSpockTest
+import pt.ulisboa.tecnico.socialsoftware.tutor.user.domain.Student
+
+import java.time.LocalDateTime
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class RemoveFailedAnswersWebServiceIT extends FailedAnswersSpockTest {
+    @LocalServerPort
+    private int port
+
+    def response
+    def courseExecution
+    def quiz
+    def quizQuestion
+    def failedAnswer
+
+    def setup() {
+        given:
+        restClient = new RESTClient("http://localhost:" + port)
+        and:
+        createExternalCourseAndExecution()
+        and:
+        student = new Student(USER_1_NAME, USER_1_EMAIL, USER_1_PASSWORD, false, AuthUser.Type.EXTERNAL)
+        student.authUser.setPassword(passwordEncoder.encode(USER_1_PASSWORD))
+        student.addCourse(externalCourseExecution)
+        userRepository.save(student)
+        and:
+        dashboard = new Dashboard(externalCourseExecution, student)
+        dashboardRepository.save(dashboard)
+        and:
+        quiz = createQuiz(1)
+        quizQuestion = createQuestion(1, quiz)
+        def questionAnswer = answerQuiz(true, false, true, quizQuestion, quiz)
+        failedAnswer = createFailedAnswer(questionAnswer, LocalDateTime.now().minusDays(8))
+    }
+
+    def "student gets failed answers from dashboard then removes it"() {
+
+    }
+
+    def "teacher can't get remove student's failed answers from dashboard"() {
+
+    }
+
+    def "student can't get another student's failed answers from dashboard"() {
+
+    }
+
+}
