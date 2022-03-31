ded5d926d46f2d6bd0e31d53c29ca4a42646f5f8
Tomás Tavares <tomastavares21122001@gmail.com>
2022-03-31 13:45:08 +0100

    basic update failed answer service call implemented #151

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/dashboard/api/FailedAnswerController.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/tutor/dashboard/api/FailedAnswerController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import pt.ulisboa.tecnico.socialsoftware.tutor.auth.domain.AuthUser;
@@ -24,9 +25,14 @@ public class FailedAnswerController {
 
     FailedAnswerController(FailedAnswerService failedAnswerService) { this.failedAnswerService = failedAnswerService; }
 
-    @GetMapping("/students/failedanswers/{dashboardId}")
+    @GetMapping("/students/dashboards/{dashboardId}/failedanswers")
     @PreAuthorize("hasRole('ROLE_STUDENT') and hasPermission(#dashboardId, 'DASHBOARD.ACCESS')")
     public Collection<FailedAnswerDto> getFailedAnswers(@PathVariable int dashboardId) {
         return this.failedAnswerService.getFailedAnswers(dashboardId);
+    }
+    
+    @PutMapping("/students/dashboards/{dashboardId}/failedanswers")
+    public void updateFailedAnswers(@PathVariable int dashboardId) {
+        this.failedAnswerService.updateFailedAnswers(dashboardId, null, null);
     }
 }
