c37c6c222f10a0b2c616b8b511cacb54bed6ece7
Tomás Tavares <tomastavares21122001@gmail.com>
2022-03-31 14:44:48 +0100

    closes #154; created student updates failed answers test

--- backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/dashboard/webservice/UpdateFailedAnswersWebServiceIT.groovy
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/dashboard/webservice/UpdateFailedAnswersWebServiceIT.groovy
@@ -10,6 +10,8 @@ import pt.ulisboa.tecnico.socialsoftware.tutor.dashboard.domain.Dashboard
 import pt.ulisboa.tecnico.socialsoftware.tutor.dashboard.service.FailedAnswersSpockTest
 import pt.ulisboa.tecnico.socialsoftware.tutor.user.domain.Student
 
+import pt.ulisboa.tecnico.socialsoftware.tutor.dashboard.dto.FailedAnswerDto;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class UpdateFailedAnswersWebServiceIT extends FailedAnswersSpockTest {
     @LocalServerPort
@@ -18,6 +20,7 @@ class UpdateFailedAnswersWebServiceIT extends FailedAnswersSpockTest {
     def response
     def quiz
     def quizQuestion
+    def questionAnswer
 
     def setup() {
         given:
@@ -25,22 +28,47 @@ class UpdateFailedAnswersWebServiceIT extends FailedAnswersSpockTest {
         and:
         createExternalCourseAndExecution()
         and:
-        student = new Student(USER_1_NAME, USER_1_EMAIL, USER_1_PASSWORD, false, AuthUser.Type.EXTERNAL)
+        student = new Student(USER_1_NAME, USER_1_USERNAME, USER_1_EMAIL, false, AuthUser.Type.EXTERNAL)
         student.authUser.setPassword(passwordEncoder.encode(USER_1_PASSWORD))
         student.addCourse(externalCourseExecution)
         userRepository.save(student)
+
         and:
         dashboard = new Dashboard(externalCourseExecution, student)
         dashboardRepository.save(dashboard)
         and:
         quiz = createQuiz(1)
         quizQuestion = createQuestion(1, quiz)
-        answerQuiz(true, false, true, quizQuestion, quiz)
+        questionAnswer = answerQuiz(true, false, true, quizQuestion, quiz)
 
     }
 
     def "student updates failed answers"() {
+        given: "student is logged in"
+        createdUserLogin(USER_1_USERNAME, USER_1_PASSWORD)
 
+        when: "the web service is invoked"
+        response = restClient.put(
+                path: '/students/dashboards/' + dashboard.getId() + '/failedanswers',
+                requestContentType: 'application/json'
+        )
+
+        then: "the request returns 200"
+        response != null
+        response.status == 200
+
+        and: "the repository contains the failed answer"
+        failedAnswerRepository.findAll().size() == 1
+
+        and: "the failed answer must be the same that was created"
+        def addedFailedAnswer = failedAnswerRepository.findAll().get(0)
+
+        addedFailedAnswer.getQuestionAnswer().getId() == questionAnswer.getId()
+        addedFailedAnswer.getQuestionAnswer().getTimeTaken() == questionAnswer.getTimeTaken()
+
+        cleanup:
+        failedAnswerRepository.deleteAll()
+        questionAnswerRepository.deleteById(questionAnswer.getId())
     }
 
     def "teacher cant update student's failed answers"() {
