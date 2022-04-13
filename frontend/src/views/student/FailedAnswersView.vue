399550ad9f12839952250bd32a1fe68ab97dfe7e
Tomás Tavares <tomastavares21122001@gmail.com>
2022-04-13 23:54:23 +0100

    added FailedAnswersView base file #184 and #185

---
+++ frontend/src/views/student/FailedAnswersView.vue
@@ -0,0 +1,21 @@
+<template>
+  <failed-answers-table></failed-answers-table>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import QuestionsView from '../teacher/questions/QuestionsView.vue';
+
+@Component({
+  components: {
+    'failed-answers-table': QuestionsView,
+  },
+})
+export default class FailedAnswersView extends Vue {
+  dashboardId: number | null = null;
+}
+</script>
+
+<style scoped>
+
+</style>
