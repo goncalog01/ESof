0cc42bf4153896e039ac90cd6e2f5c900c421039
Sebastiao-Assuncao <sebastiao.assuncao@gmail.com>
2022-04-19 18:41:36 +0100

    closes #210; Implemented base table in DifficultQuestionView

--- frontend/src/views/student/DifficultQuestionsView.vue
+++ frontend/src/views/student/DifficultQuestionsView.vue
@@ -1,18 +1,59 @@
 <template>
-  <difficult-questions-table></difficult-questions-table>
+  <v-container fluid>
+    <v-card class="table">
+      <v-row>
+        <v-col>
+          <h1>Difficult Questions</h1>
+        </v-col>
+      </v-row>
+
+      <v-data-table
+        :headers="headers"
+        :items="difficultQuestions"
+        :sort-by="['percentage']"
+        sort-desc
+        :mobile-breakpoint="0"
+        :items-per-page="10"
+      >
+      </v-data-table>
+    </v-card>
+  </v-container>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import QuestionsView from '@/views/teacher/questions/QuestionsView.vue';
+import { Component, Vue, Watch } from 'vue-property-decorator';
+import Question from '@/models/management/Question';
+import DifficultQuestion from '@/models/dashboard/DifficultQuestion';
 
 @Component({
-  components: {
-    'difficult-questions-table': QuestionsView,
-  },
+  components: {},
 })
 export default class DifficultQuestionsView extends Vue {
   dashboardId: number | null = null;
+  difficultQuestions: DifficultQuestion[] = [];
+
+  headers: object = [
+    {
+      text: 'Actions',
+      value: 'action',
+      align: 'left',
+      width: '5px',
+      sortable: false,
+    },
+    {
+      text: 'Question',
+      value: 'questionAnswerDto.question.title',
+      width: '80%',
+      align: 'left',
+      sortable: false,
+    },
+    {
+      text: 'Percentage',
+      value: 'collected',
+      width: '10%',
+      align: 'center',
+    },
+  ];
 }
 </script>
 
