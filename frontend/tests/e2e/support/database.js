c47ede1155f1249e66bc3da1292854509b5aaf98
goncalog01 <goncalog01@gmail.com>
2022-04-21 18:32:58 +0100

    closes #245; extented database.js file

--- frontend/tests/e2e/support/database.js
+++ frontend/tests/e2e/support/database.js
@@ -139,10 +139,40 @@ Cypress.Commands.add('cleanCodeFillInQuestionsByName', (questionName) => {
               DELETE FROM questions WHERE id IN (SELECT question_id FROM toDelete);`);
 });
 
+Cypress.Commands.add('createWeeklyScore', () => {
+  dbCommand(`WITH courseExecutionId as (SELECT ce.id as course_execution_id FROM course_executions ce WHERE acronym = 'DemoCourse')
+        , demoStudentId as (SELECT u.id as users_id FROM users u WHERE name = 'Demo Student')
+        , dashboardId as (SELECT d.id as dashboard_id FROM dashboard d WHERE student_id = (select users_id from demoStudentId) AND course_execution_id = (select course_execution_id from courseExecutionId))
+       INSERT INTO weekly_score(closed, number_answered, percentage_correct, uniquely_answered, week, dashboard_id) VALUES (true, 10, 50, 9, '2022-02-02', (select dashboard_id from dashboardId))
+      `);
+});
+
+Cypress.Commands.add('deleteWeeklyScores', () => {
+  dbCommand(`
+         UPDATE dashboard SET last_check_weekly_scores = NULL;
+         DELETE FROM weekly_score;
+    `);
+});
+
+Cypress.Commands.add('setFailedAnswersAsOld', () => {
+  dbCommand(`WITH courseExecutionId as (SELECT ce.id as course_execution_id FROM course_executions ce WHERE acronym = 'DemoCourse')
+                , demoStudentId as (SELECT u.id as users_id FROM users u WHERE name = 'Demo Student')
+                , dashboardId as (SELECT d.id as dashboard_id FROM dashboard d WHERE student_id = (select users_id from demoStudentId) AND course_execution_id = (select course_execution_id from courseExecutionId))
+  UPDATE failed_answer SET collected='2022-02-02'
+  `);
+});
+
 Cypress.Commands.add('deleteFailedAnswers', () => {
   dbCommand(`
          UPDATE dashboard SET last_check_failed_answers = NULL;
          DELETE FROM failed_answer;
+    `);
+});
+
+Cypress.Commands.add('deleteDifficultQuestions', () => {
+  dbCommand(`
+         UPDATE dashboard SET last_check_difficult_questions = NULL;
+         DELETE FROM difficult_question;
     `);
 });
 
@@ -158,13 +188,5 @@ Cypress.Commands.add('deleteQuestionsAndAnswers', () => {
          DELETE FROM options;
          DELETE FROM question_details;
          DELETE FROM questions;
-    `);
-});
-
-Cypress.Commands.add('setFailedAnswersAsOld', () => {
-  dbCommand(`WITH courseExecutionId as (SELECT ce.id as course_execution_id FROM course_executions ce WHERE acronym = 'DemoCourse')
-        , demoStudentId as (SELECT u.id as users_id FROM users u WHERE name = 'Demo Student')
-        , dashboardId as (SELECT d.id as dashboard_id FROM dashboard d WHERE student_id = (select users_id from demoStudentId) AND course_execution_id = (select course_execution_id from courseExecutionId))
-        UPDATE failed_answer SET collected='2022-02-02' 
     `);
 });
